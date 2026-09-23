3e1a49bc8430d64ddd9b2dcf091410e48a6fcf1c
SandraL34 <s.lorbois@gmail.com>
2026-09-23 16:05:56 +0200

    Adaptation token JWT

--- src/api_client.h
+++ src/api_client.h
@@ -1,16 +1,24 @@
 #ifndef PROJETRSP_API_CLIENT_H
 #define PROJETRSP_API_CLIENT_H
-
+#include <Arduino.h>
 
 class ApiClient {
-    public:
-        ApiClient(const char* apiUrl);
+public:
+    ApiClient(
+        const char* apiUrl,
+        const char* username,
+        const char* password
+    );
 
-        void sendMeasurement(int light, float distance, bool panelOpen);
+    bool login();
+    void sendMeasurement(int light, float distance, bool panelOpen);
 
-    private:
-        const char* _apiUrl;
+private:
+    const char* _apiUrl;
+    const char* _username;
+    const char* _password;
+
+    String _token;
 };
 
-
-#endif //PROJETRSP_API_CLIENT_H
+#endif // PROJETRSP_API_CLIENT_H
